b98aa1402b7bd19d3fe427549f74144031ba1d8a
riccardo-zotti <zottiriccardo08@gmail.com>
2025-12-11 09:49:29 +0100

    INIZIO ES- 3

--- 1-string/3_userGenerator.cpp
+++ 1-string/3_userGenerator.cpp
@@ -25,20 +25,26 @@ string minuscolo(string s){
           prima lettera del nome + tutto il cognome (tutto minuscolo)
 */
 string usernameGenerator(string nome, string cognome){
-    return "da implementare";
+    string nc=nome[0]+cognome;
+    return nc;
 }
 
 /*
     TODO: Implementare la funzione affinchè, a partire da 1 stringa nella forma "Nome Cognome"
           generi in output una stringa contenente uno username utente nella forma:
           prima lettera del nome + tutto il cognome (tutto minuscolo)
-    
+
     Hint: rispetto alla funzione precedente, splittare (spezzare il nome dal cognome, basandosi sullo spazio)
           e ripetere la stessa logica. E' possibile anche evitare di ripetere la logica? Magari chiamando la stessa funzione sopra?
 
 */
 string usernameGenerator(string nomeCognome){
-    return "da implementare";
+    int x=nomeCognome.find(" ");
+    string n=nomeCognome.substr(0, x);
+    string c=nomeCognome.substr(x, nomeCognome.length());
+    cout<< "il nome e': " <<n<< endl;
+    cout<<"il cognome e': "<<c<<endl;
+    return 0;
 }
 
 // main con testing
@@ -54,8 +60,8 @@ int main()
 
     string username2 = usernameGenerator(nomeCognome);
 
-    cout << "Primo utente: " << nome << " " << cognome << ". Username: " << username1;
-    cout << "Secondo utente: " << nomeCognome << ". Username: " << username2;
+    cout << "Primo utente: " << nome << " " << cognome << ". Username: " << username1<<endl;
+    cout << "Secondo utente: " << nomeCognome << ". Username: " << username2<<endl;
 
     return 0;
 }
