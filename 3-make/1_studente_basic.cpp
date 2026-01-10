fa0bae7df4be78b678798fcac78c7ea57c5a496e
riccardo-zotti <zottiriccardo08@gmail.com>
2026-01-10 11:55:12 +0100

    Es studente basic svolto

--- 3-make/1_studente_basic.cpp
+++ 3-make/1_studente_basic.cpp
@@ -21,11 +21,22 @@ int main()
 {
     /*********************** Creazione di oggetti ******************************/
     // TODO Creare un nuovo studente "Mario" con eta 17 e media 7
-    // TODO Creare un nuovo studente "Maria"  (questa volta con un puntatore) con eta 18 e media 8
+    Studente MO;
+    MO.nome="Mario";
+    MO.eta=17;
+    MO.media=7.00;
 
+    // TODO Creare un nuovo studente "Maria"  (questa volta con un puntatore) con eta 18 e media 8
+    Studente MA;
+    Studente *p=&MA;;
+
+    p->nome="Maria";
+    p->eta=18;
+    p->media=8.00;
 
     /************************** Visualizzazione ********************************/
     // TODO Visualizzare il nome e la media sia di Mario che Maria
-
+    MO.stampaStudente();
+    p->stampaStudente();
     return 0;
 }
